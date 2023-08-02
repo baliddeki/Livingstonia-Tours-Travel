94891c57fbcfd8facbad7ee7b563158ea3957433
Bryan <baliddeki@gmail.com>
2023-08-03 02:53:03 +0300

    added tour-package route

--- livingstonia_tours/routes/web.php
+++ livingstonia_tours/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TourServiceController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing_page');
 });
+
+
+Route::get('/tour_packages', [TourServiceController::class, 'index'])->name('tour-packages.index');
